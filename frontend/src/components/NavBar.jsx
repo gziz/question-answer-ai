414de329f83c4b37f5870105d8167372dcbfb4a5
gziz <69287011+gziz@users.noreply.github.com>
2022-08-20 16:11:52 -0500

    Fix font color "dark"

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -18,10 +18,10 @@ const NavBar = (props) => {
     return (                
             <div className="fixed top-0 w-screen h-14
                             flex flex-row space-x-10
-                            dark:bg-slate-800 p-6
+                            bg-slate-800 p-6
                             justify-start">
 
-                <span className="self-center text-xl font-semibold dark:text-white">Q&A</span>
+                <span className="self-center text-xl font-semibold text-white">Q&A</span>
                 <button onClick={() => updateContextType("text")} className="self-center text-gray-300 hover:text-white">Context As Text</button>
                 <button onClick={() => updateContextType("url")} className="self-center text-gray-300 hover:text-white">Context As Url</button>
             </div>
