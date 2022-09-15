8a44dd28f1d1411606d74423f6c7d87e3962a88a
gziz <69287011+gziz@users.noreply.github.com>
2022-09-15 00:17:48 -0500

    local endpoint

--- frontend/src/apis/axiosManager.js
+++ frontend/src/apis/axiosManager.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_BASEURL
+//const baseURL = process.env.REACT_APP_BASEURL
 
+const baseURL = 'http://127.0.0.1:8000';
 export default axios.create({
     baseURL,
 });
