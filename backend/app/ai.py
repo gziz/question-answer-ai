6832b5185ed65c8e8219c17499a8e922a0e253a1
gziz <69287011+gziz@users.noreply.github.com>
2022-10-07 12:52:16 -0500

    Modified ai.py model handler

--- backend/app/ai.py
+++ backend/app/ai.py
@@ -3,7 +3,7 @@ np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
 
 import pathlib
 
-from transformers import BertForQuestionAnswering
+from transformers import AutoModelForQuestionAnswering
 from transformers import AutoTokenizer
 from transformers import pipeline
 
@@ -12,7 +12,7 @@ BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
 QA_MODEL_PATH = BASE_DIR / "ai-models" / "roberta-base-squad2"
 
 def get_model():
-    model = BertForQuestionAnswering.from_pretrained(QA_MODEL_PATH)
+    model = AutoModelForQuestionAnswering.from_pretrained(QA_MODEL_PATH)
     tokenizer = AutoTokenizer.from_pretrained(QA_MODEL_PATH)
     nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
 
