aeb14e3ef1fd7c18f5c5d9f09b61c376f27d8d6f
gziz <69287011+gziz@users.noreply.github.com>
2023-03-09 18:48:47 -0600

    Vm unavailable popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,35 @@ import QuestionFile from './components/QuestionFile';
 import QuestionText from './components/QuestionText';
 import Navbar from "./components/NavBar";
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+import { useEffect, useState } from 'react';
+import Modal from './components/Modal';
+
+
+
 
 function App() {
+
+  const [timedPopup, setTimedPopup] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimedPopup(true);
+    }, 5000);  
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
       <Router>
         <Navbar/>
+        <Modal trigger={timedPopup} setTrigger={setTimedPopup} />
 
         <Routes>
           <Route exact path="/" element = {<QuestionText/>}/>
           <Route exact path="/file" element = {<QuestionFile/>}/>
         </Routes>
       </Router>
+
       </header>
     </div>
   );
---
+++ frontend/src/components/Popup.jsx
@@ -0,0 +1,16 @@
+
+function Modal(props) {
+  return (props.trigger) ? (
+    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center z-10">
+      <div className="bg-white p-2 rounded w-72 justify-between p-6">
+        <h1 className="font-semibold text-center text-xl text-gray-700">
+          Site currently unavailable
+        </h1>
+        <p className="text-justify text-gray-700 mb-5">The machine hosting the AI model is currently unavailable!</p>
+        
+      </div>
+    </div>
+  ): "";
+}
+
+export default Modal;
